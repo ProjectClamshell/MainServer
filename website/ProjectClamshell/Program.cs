e77ab17515c001b81afa5d9a8cfca2401e8667c6
Karaleena333 <karaleenadalton@gmail.com>
2026-03-27 12:41:24 -0230

    last one didnt work, now it should

--- website/ProjectClamshell/Program.cs
+++ website/ProjectClamshell/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient();
+
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
