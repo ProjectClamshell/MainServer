027d7476dc4b756c7437cdc8deb74a93c3e61281
Karaleena333 <karaleenadalton@gmail.com>
2026-03-29 10:24:33 -0230

    Current page on website now displays the latest message information

--- Server/src/ClamShellServer/Database.cs
+++ Server/src/ClamShellServer/Database.cs
@@ -8,10 +8,10 @@ public interface DatabaseConnection
     Task<IEnumerable<Message>> GetMessageByPGN(string requestedPGN);
     Task<IEnumerable<Message>> GetMessageByTimePGN(int requestedTime, string requestedPGN);
     Task<IEnumerable<int>> GetTotalMessagesAsync();
-    Task<IEnumerable<int>> GetSignedMessagesAsync();
-    Task<IEnumerable<int>> GetUnSignedMessagesAsync();
-    Task<IEnumerable<int>> GetValidatedMessagesAsync();
-    Task<IEnumerable<int>> GetUnValidatedMessagesAsync();
+    Task<IEnumerable<Message>> GetSignedMessagesAsync();
+    Task<IEnumerable<Message>> GetUnSignedMessagesAsync();
+    Task<IEnumerable<Message>> GetValidatedMessagesAsync();
+    Task<IEnumerable<Message>> GetUnValidatedMessagesAsync();
     Task<int> ResetTableAsync();
     Task SaveMessageAsync(string content, bool signed, bool validated, string pgn);}
 public class Database : DatabaseConnection
@@ -48,28 +48,28 @@ public class Database : DatabaseConnection
         return await connection.QueryAsync<int>("SELECT count(*) FROM messages");
     }
 
-    public async Task<IEnumerable<int>> GetSignedMessagesAsync()
+    public async Task<IEnumerable<Message>> GetSignedMessagesAsync()
     {
         await using var connection = new NpgsqlConnection(_connectionString);
-        return await connection.QueryAsync<int>("SELECT count(*) FROM messages where signed = True");
+        return await connection.QueryAsync<Message>("SELECT * FROM messages where signed = True");
     }
 
-    public async Task<IEnumerable<int>> GetUnSignedMessagesAsync()
+    public async Task<IEnumerable<Message>> GetUnSignedMessagesAsync()
     {
         await using var connection = new NpgsqlConnection(_connectionString);
-        return await connection.QueryAsync<int>("SELECT count(*) FROM messages where signed = False");
+        return await connection.QueryAsync<Message>("SELECT * FROM messages where signed = False");
     }
 
-    public async Task<IEnumerable<int>> GetValidatedMessagesAsync()
+    public async Task<IEnumerable<Message>> GetValidatedMessagesAsync()
     {
         await using var connection = new NpgsqlConnection(_connectionString);
-        return await connection.QueryAsync<int>("SELECT count(*) FROM messages where validated = True");
+        return await connection.QueryAsync<Message>("SELECT * FROM messages where validated = True");
     }
 
-    public async Task<IEnumerable<int>> GetUnValidatedMessagesAsync()
+    public async Task<IEnumerable<Message>> GetUnValidatedMessagesAsync()
     {
         await using var connection = new NpgsqlConnection(_connectionString);
-        return await connection.QueryAsync<int>("SELECT count(*) FROM messages where validated = False");
+        return await connection.QueryAsync<Message>("SELECT * FROM messages where validated = False");
     }
 
     public async Task<int> ResetTableAsync()
