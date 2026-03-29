70e1f22d1488759b5144d33aa68d8ce95a70f09f
Karaleena333 <karaleenadalton@gmail.com>
2026-03-28 22:24:20 -0230

    This is the website with the API calls for server status and also the current count

---
+++ website/ProjectClamshell/ViewModels/Models.cs
@@ -0,0 +1,12 @@
+namespace ProjectClamshell.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string? Content { get; set; }
+        public DateTime ReceivedAt { get; set; }
+        public string? PGN { get; set; }
+    }
+}
+
+
